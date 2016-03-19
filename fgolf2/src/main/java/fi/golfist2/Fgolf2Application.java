c63882a9a0c87f907677b08258136899ba42eb5a
TV <ab@cde.com>
2016-03-19 16:07:49 +0200

    Initial commit

---
+++ fgolf2/src/main/java/fi/golfist2/Fgolf2Application.java
@@ -0,0 +1,12 @@
+package fi.golfist2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fgolf2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fgolf2Application.class, args);
+	}
+}
