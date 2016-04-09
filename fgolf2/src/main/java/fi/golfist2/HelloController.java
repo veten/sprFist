b656b636655b05832beea7662c580e936195a667
TV <ab@cde.com>
2016-04-10 01:11:54 +0300

    email NOT working

--- fgolf2/src/main/java/fi/golfist2/HelloController.java
+++ fgolf2/src/main/java/fi/golfist2/HelloController.java
@@ -1,19 +1,48 @@
 package fi.golfist2;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@RestController
+@Controller
 public class HelloController {
 
-    @RequestMapping("/")
+	@RequestMapping("/")
     public String index() {
-        return "This is where it starts! Check: '/underdog' ";
+        return "index";
     }
 
-    @RequestMapping("/underdog")
-    public String underdog() {
-        return "Put some stuff here! And more...";
+	@RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+	
+	@RequestMapping("/secret")
+    public String secret() {
+        return "secretStuff";
+    }
+	
+    @RequestMapping(value="/register", method=RequestMethod.GET)
+    public String registerForm(Model model) {
+        model.addAttribute("user", new NewUser());
+        return "register";
+    }
+    
+    
+    @RequestMapping(value="/register", method=RequestMethod.POST)
+    public String formSubmit(@ModelAttribute NewUser user, Model model) {
+    	UserControllerUtil.registerSubmit();
+        System.out.println(user.getName());
+        System.out.println(user.getEmail());
+        model.addAttribute("user", user);
+        return "result";
     }
 
     
