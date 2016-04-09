2ee8993c1e6883631a3d8cd6a4426d7b9de5106a
pk <pk@linux-paiva>
2016-04-10 01:23:04 +0300

    local scheisse commit

--- fgolf2/src/main/java/fi/golfist2/Fgolf2Application.java
+++ fgolf2/src/main/java/fi/golfist2/Fgolf2Application.java
@@ -2,10 +2,18 @@ package fi.golfist2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Fgolf2Application {
+public class Fgolf2Application extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Fgolf2Application.class);
+	}
+
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Fgolf2Application.class, args);
 	}
--- fgolf2/src/main/java/fi/golfist2/Hello2Controller.java
+++ fgolf2/src/main/java/fi/golfist2/Hello2Controller.java
@@ -1,14 +1,34 @@
 package fi.golfist2;
 
+import java.net.URL;
+import java.net.URLClassLoader;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.io.*;
 
 @Controller
 public class Hello2Controller 
 {
-	@RequestMapping("/sepi")
-	public String sepi()
+	@RequestMapping("/sepi2")
+	public String sepi(Model model) throws IOException
 	{
+//		return "sepi";
+		printClasspath();
+		System.out.println(new java.io.File( "." ).getCanonicalPath());
 		return "sepi";
 	}
+	
+	private static void printClasspath () {
+
+        ClassLoader cl = ClassLoader.getSystemClassLoader();
+
+        URL[] urls = ((URLClassLoader)cl).getURLs();
+
+        for(URL url: urls){
+        	System.out.println(url.getFile());
+        }
+         
+   }
 }
